fb74ebdf405889d420cb5b9743b5617110c64077
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-07-27 18:37:23 +0700

    firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBUHrynLM_optOeIXSvBOzvI-MZhsQh068",
+    authDomain: "carirumah-45009.firebaseapp.com",
+    databaseURL: "https://carirumah-45009.firebaseio.com",
+    projectId: "carirumah-45009",
+    storageBucket: "carirumah-45009.appspot.com",
+    messagingSenderId: "693309178392",
+    appId: "1:693309178392:web:26340b65c913cc5534b96d",
+    measurementId: "G-FXHCRVLE5X"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+
+  export {firebaseApp, db}

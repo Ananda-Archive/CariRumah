424db1a405d69381214ea43f9cc237b5ad28628f
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-07-27 18:38:08 +0700

    add dashboard page

---
+++ src/views/admin/Dashboard.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app>
+        <v-content app class="mx-4 my-7 mx-md-10 my-md-n5">
+            <div>
+                <v-row justify="center">
+                    <!-- Search Feature -->
+                    <v-col cols="12">
+                        <v-text-field
+                            outlined
+                            dense
+                            v-model="search"
+                            label="Cari Data"
+                            prepend-inner-icon="mdi-magnify"
+                            :clearable="true"
+                            @click:clear="search = ''"
+                        ></v-text-field>
+                    </v-col>
+                    <!-- List -->
+                    <!-- Add -->
+                    <v-btn fab dark large color="primary" fixed right bottom>
+                        <v-icon dark>mdi-plus</v-icon>
+                    </v-btn>
+                </v-row>
+            </div>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard',
+
+    data() {
+        return {
+            search:''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

3d5036980cc77acd0bfe67d6e64c174474782829
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-07-27 18:36:25 +0700

    add footer copyright and such thing

---
+++ src/components/FooterLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-footer
+        class="justify-center"
+        color="#292929"
+        height="40"
+    >
+        <div class="subtitle-1 font-weight-light grey--text text--lighten-1 text-center">
+            &copy; {{ (new Date()).getFullYear() }} — CariRumah
+        </div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterLayout'
+}
+</script>
+
+<style>
+
+</style>

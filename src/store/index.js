89d8d9ffb655251fb88443533156201a35e86469
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-07-27 18:37:03 +0700

    add uid state

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import province from './modules/province'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    uid:'',
   },
   mutations: {
+    setUid (state, id) {
+      state.uid = id
+    },
   },
   actions: {
+    mutateUid( {commit}, id ) {
+      commit('setUid', id)
+    },
   },
   modules: {
     province
-  }
+  },
+  plugins: [createPersistedState()]
 })

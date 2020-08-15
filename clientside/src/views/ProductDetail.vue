52e09453e564f05bd80ac47b18fc9f12f265d1e2
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-08-15 09:46:49 +0700

    add tinybox

--- clientside/src/views/ProductDetail.vue
+++ clientside/src/views/ProductDetail.vue
@@ -1,13 +1,13 @@
 <template>
     <v-app>
-        <v-content class="ma-md-12">
+        <v-content class="my-md-12 mx-md-16 px-md-16">
             <v-container fluid>
                 <v-row justify-center>
                     <v-col cols="12" class="text-center">
                         <v-row no-gutter>
                             <v-col :cols="[product.images.length>1? '7' : '12']">
                                 <v-card width="100%">
-                                    <v-img :height="[$vuetify.breakpoint.xl? '500px' : '400px']" :src=product.images[0].image :lazy-src=product.images[0].image>
+                                    <v-img :height="[$vuetify.breakpoint.xl? '500px' : '400px']" :src=product.images[0].image :lazy-src=product.images[0].image @click="index = 0">
                                         <template v-slot:placeholder>
                                             <v-row
                                                 class="fill-height ma-0"
@@ -22,7 +22,13 @@
                             </v-col>
                             <v-col cols="5" v-if="product.images.length > 1">
                                 <v-card width="100%" v-for="(n,idx) in 2" :key="idx" class="marginCard">
-                                    <v-img :height="[$vuetify.breakpoint.xl? '240px' : '190px']" :src=product.images[n].image :lazy-src=product.images[n].image></v-img>
+                                    <v-img :height="[$vuetify.breakpoint.xl? '240px' : '190px']" :src=product.images[n].image :lazy-src=product.images[n].image @click="index = n" class="align-end">
+                                        <v-container fluid full-height>
+                                            <v-row>
+                                                <v-col cols="12" class="text-right"><v-btn v-if="n == 2" depressed class="primary" dark @click.stop="index = 0">Lihat Semua Foto ({{productImages.length}})</v-btn></v-col>
+                                            </v-row>
+                                        </v-container>
+                                    </v-img>
                                 </v-card>
                             </v-col>
                         </v-row>
@@ -30,6 +36,11 @@
                 </v-row>
             </v-container>
         </v-content>
+        <Tinybox
+            v-model="index"
+            :images="productImages"
+            loop
+        />
     </v-app>
 </template>
 
@@ -37,13 +48,19 @@
 
 import api from '@/api'
 import _ from 'lodash'
+import Tinybox from "vue-tinybox"
+
 
 export default {
+    components: { Tinybox },
     data() {
         return {
+            index:null,
             products:[],
             product:{},
+            productImages:[],
             loadingState: false,
+            overlay: true,
         }
     },
 
@@ -62,6 +79,9 @@ export default {
                 .then((response) => {
                     this.products = response
                     this.product = _.find(this.products, ['id',this.$route.params.id])
+                    this.product.images.forEach(e => {
+                        this.productImages.push(e.image)
+                    });
                     this.loadingState = false
                 })
         },

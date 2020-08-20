a658e05932bf498ed904d6164f1e5678dc6454bb
anandavj <anandaprabu.trityavijaya@gmail.com>
2020-08-20 08:39:24 +0700

    add about page

---
+++ clientside/src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+        <whatsapp-floating-button></whatsapp-floating-button>
+        <v-main app class="ma-md-12 ma-8">
+            <div v-html="about"></div>
+        </v-main>
+        <footer-layout></footer-layout>
+    </v-app>
+</template>
+
+<script>
+
+import api from '@/api'
+import WhatsappFloatingButton from '@/components/WhatsAppFloatingButton.vue'
+import FooterLayout from '@/components/FooterLayout.vue'
+
+export default {
+    components: {
+        WhatsappFloatingButton,
+        FooterLayout
+    },
+
+    data() {
+        return {
+            about:''
+        }
+    },
+
+    mounted() {
+        api.getAllAbout() 
+            .then((response) => {
+                this.about = response[0].about
+            })
+    },
+}
+</script>
+
+<style>
+
+</style>
